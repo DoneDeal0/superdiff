63c45e9ff636686ddbb7af06fe77792bfbed1979
DoneDeal0 <ap.lanoe@outlook.com>
2024-01-21 11:24:04 +0100

    feat: allow referenceProperty for object comparison in lists (#15)
    * feat: allow referenceProperty for object comparison in lists

--- src/list-diff.ts
+++ src/list-diff.ts
@@ -5,7 +5,7 @@ import {
   ListDiffStatus,
   ListOptions,
 } from "./model";
-import { isEqual } from "./utils";
+import { isEqual, isObject } from "./utils";
 
 function getLeanDiff(
   diff: ListDiff["diff"],
@@ -14,12 +14,12 @@ function getLeanDiff(
   return diff.filter((value) => showOnly?.includes(value.status));
 }
 
-function formatSingleListDiff(
-  listData: ListData[],
+function formatSingleListDiff<T>(
+  listData: T[],
   status: ListDiffStatus,
   options: ListOptions = { showOnly: [] }
 ): ListDiff {
-  const diff = listData.map((data: ListData, i) => ({
+  const diff = listData.map((data, i) => ({
     value: data,
     prevIndex: status === LIST_STATUS.ADDED ? null : i,
     newIndex: status === LIST_STATUS.ADDED ? i : null,
@@ -46,10 +46,26 @@ function getListStatus(listDiff: ListDiff["diff"]): ListDiffStatus {
     : LIST_STATUS.EQUAL;
 }
 
-export const getListDiff = (
-  prevList: ListData[] | undefined | null,
-  nextList: ListData[] | undefined | null,
-  options: ListOptions = { showOnly: [] }
+function isReferencedObject(
+  value: any,
+  referenceProperty: ListOptions["referenceProperty"]
+): value is Record<string, any> {
+  if (isObject(value) && !!referenceProperty) {
+    return Object.hasOwn(value, referenceProperty);
+  }
+  return false;
+}
+
+/**
+ * Returns the diff between two arrays
+ * @param {Array<T>} prevList - The original array.
+ * @param {Array<T>} nextList - The new array.
+ * @returns ListDiff
+ */
+export const getListDiff = <T>(
+  prevList: T[] | undefined | null,
+  nextList: T[] | undefined | null,
+  options: ListOptions = { showOnly: [], referenceProperty: undefined }
 ): ListDiff => {
   if (!prevList && !nextList) {
     return {
@@ -59,37 +75,47 @@ export const getListDiff = (
     };
   }
   if (!prevList) {
-    return formatSingleListDiff(
-      nextList as ListData,
-      LIST_STATUS.ADDED,
-      options
-    );
+    return formatSingleListDiff(nextList as T[], LIST_STATUS.ADDED, options);
   }
   if (!nextList) {
-    return formatSingleListDiff(
-      prevList as ListData,
-      LIST_STATUS.DELETED,
-      options
-    );
+    return formatSingleListDiff(prevList as T[], LIST_STATUS.DELETED, options);
   }
   const diff: ListDiff["diff"] = [];
   const prevIndexMatches: number[] = [];
   nextList.forEach((nextValue, i) => {
-    const prevIndex = prevList.findIndex(
-      (prevValue, prevIdx) =>
+    const prevIndex = prevList.findIndex((prevValue, prevIdx) => {
+      if (isReferencedObject(prevValue, options.referenceProperty)) {
+        if (isObject(nextValue)) {
+          return (
+            isEqual(
+              prevValue[options.referenceProperty as string],
+              nextValue[options.referenceProperty as string]
+            ) && !prevIndexMatches.includes(prevIdx)
+          );
+        }
+        return false;
+      }
+      return (
         isEqual(prevValue, nextValue) && !prevIndexMatches.includes(prevIdx)
-    );
+      );
+    });
     if (prevIndex > -1) {
       prevIndexMatches.push(prevIndex);
     }
     const indexDiff = prevIndex === -1 ? null : i - prevIndex;
     if (indexDiff === 0) {
+      let nextStatus = LIST_STATUS.EQUAL;
+      if (isReferencedObject(nextValue, options.referenceProperty)) {
+        if (!isEqual(prevList[prevIndex], nextValue)) {
+          nextStatus = LIST_STATUS.UPDATED;
+        }
+      }
       return diff.push({
         value: nextValue,
         prevIndex,
         newIndex: i,
         indexDiff,
-        status: LIST_STATUS.EQUAL,
+        status: nextStatus,
       });
     }
     if (prevIndex === -1) {
