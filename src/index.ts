d18bfea77d5e9ecc6fef62dccf69e0d433a3e4d4
Máté Szentgróti <mszentgroti@hearsaycorp.com>
2024-06-23 17:48:24 +0200

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 export { getObjectDiff } from "./object-diff";
 export { getListDiff } from "./list-diff";
 export { isEqual, isObject } from "./utils";
+export {
+    type DataDiff,
+    type ListData,
+    type ListDiff,
+    type ListDiffStatus,
+    type ListOptions,
+    type ListStatusTuple,
+    type ObjectData,
+    type ObjectDiff,
+    type ObjectDiffStatus,
+    type ObjectOptions,
+    type ObjectStatusTuple,
+    type SubProperties,
+    type isEqualOptions
+} from "./model";
